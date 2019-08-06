1c263b626f7ab49a213f1bc1024091413ab4cf4f
Aymen Rebouh <aymenmse@gmail.com>
2019-08-06 12:21:48 +0900

    Initial commit

---
+++ PracticeRx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticeRx
+//
+//  Created by Aymen Rebouh on 2019/08/06.
+//  Copyright © 2019 Aymenworks. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
